29b8e9d7911d2bc5de1adaf912decc5cae7716a1
nick <nickbarry@163.com>
2021-07-23 17:45:53 +0800

    1、实现简单index.html的拦截器，防止直接访问到index.html界面，直接访问会跳转回login.html界面并提示登录失败信息 2、实现简单账号密码的判断登录，如果输入用户名和“123456”密码，则判断登录成功，否则页面提示登录失败信息 3、获取登录的用户名并且显示到index主界面中

---
+++ src/main/java/com/example/web/bean/User.java
@@ -0,0 +1,15 @@
+package com.example.web.bean;
+
+import lombok.Data;
+
+/**
+ * @Author Nick
+ * @Classname User
+ * @Date 2021/7/23 17:21
+ * @Description
+ */
+@Data
+public class User {
+    private String userName;
+    private String passWord;
+}
--- src/main/java/com/example/web/controller/IndexController.java
+++ src/main/java/com/example/web/controller/IndexController.java
@@ -1,7 +1,13 @@
 package com.example.web.controller;
 
+import com.example.web.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @Author Nick
@@ -11,8 +17,44 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class IndexController {
+    /**
+     * 登录页配置
+     * @return
+     */
     @GetMapping(value = {"/","index","login"})
-    public String indexLogin(){
+    public String loginPage(){
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(User user, HttpSession httpSession, Model model){
+
+        if(StringUtils.hasLength(user.getUserName()) && "123456".equals(user.getPassWord())){
+            //把登录成功的用户保存起来
+            httpSession.setAttribute("loginUser",user);
+            //登录成功后重定向到index.html界面，防止重复提交login界面表单
+            return "redirect:index.html";
+        }else {
+            model.addAttribute("msg","账号或密码输入错误，请重新尝试！");
+            return "login";
+        }
+    }
+
+    /**
+     * 主页
+     * @return
+     */
+    @GetMapping("index.html")
+    public String mainPage(HttpSession session,Model model){
+
+        //是否登录      拦截器
+        Object loginUser = session.getAttribute("loginUser");
+        if(loginUser != null){
+            return "index";
+        }else {
+            //登录不成功，回到登录login界面
+            model.addAttribute("msg","请重新登录！");
+            return "login";
+        }
     }
 }
