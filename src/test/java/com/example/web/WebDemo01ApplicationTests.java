7d7d066e4da0d0df9e416f9dfbd401e4d35bcd70
nick <nickbarry@163.com>
2021-07-29 16:23:07 +0800

    1、添加了mysql的jdbc连接 2、数据库用的mysql 5.x.xx版本，所以需要更改springboot依赖中的默认版本号 3、添加测试用例测试能否查询到本地数据库

--- src/test/java/com/example/web/WebDemo01ApplicationTests.java
+++ src/test/java/com/example/web/WebDemo01ApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Slf4j
 @SpringBootTest
 class WebDemo01ApplicationTests {
 
+//    自动注入
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test
     void contextLoads() {
+//        jdbcTemplate.queryForList("select * from user ");
+//        查询到test表中只有一个属性test，且只有一个值为123，返回给long类型的along变量并日志输出
+        long along = jdbcTemplate.queryForObject("SELECT * FROM test",long.class);
+        log.info("查询到的长度   {}",along);
     }
 
 }
